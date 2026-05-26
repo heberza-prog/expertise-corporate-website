89b02b9055bcc1ae3de128fc1307bd6c45800e51
heberza-prog <heberza@gmail.com>
2026-05-26 17:44:39 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,45 @@
+/* =========================
+   DARK MODE
+========================= */
+
 function toggleTheme(){
+
 document.body.classList.toggle("dark");
+
 }
+
+/* =========================
+   SCROLL REVEAL
+========================= */
+
+const reveals =
+document.querySelectorAll(
+".reveal, .reveal-right"
+);
+
+function revealOnScroll(){
+
+reveals.forEach((el)=>{
+
+const top =
+el.getBoundingClientRect().top;
+
+const windowHeight =
+window.innerHeight;
+
+if(top < windowHeight - 100){
+
+el.classList.add("active");
+
+}
+
+});
+
+}
+
+window.addEventListener(
+"scroll",
+revealOnScroll
+);
+
+revealOnScroll();
